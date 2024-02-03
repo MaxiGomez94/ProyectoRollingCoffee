edc37a51482c28d5295b887b8a293fde25a2f127
Maximiliano Gomez <alexander34223@gmail.com>
2024-02-03 01:44:47 -0300

    agregue el navbar y el logo

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../../assets/Coffee_Logo.png";
 
 const menu = () => {
-    return (
-        <div>
-            <p>aqui va el navbar</p>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary ">
+        <Container>
+          <Navbar.Brand href="">
+            <img src={logo} alt="RollingCode Logo nav" className="img-fluid d-flex justify-content-between" width={200}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Inicio</Nav.Link>
+              <Nav.Link href="#link">Administrador</Nav.Link>
+              <Nav.Link href="#link">Registro</Nav.Link>
+              <Nav.Link href="#link">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default menu;
