94c72877fd48e180710411e9e5f404f2a2bb0972
Maximiliano Gomez <alexander34223@gmail.com>
2024-02-03 00:22:30 -0300

    estructura limpia para el proyecto con imagenes en assets

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+const App=()=> {
+
+
+  return (
+    <>
+    <h1 className='text-center'>hola</h1>
+
+    </>
+  )
+}
+
+export default App
