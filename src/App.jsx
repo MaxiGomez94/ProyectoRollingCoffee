f0239626f6b158899b1a0d7fc93fb6c5301132de
Maximiliano Gomez <alexander34223@gmail.com>
2024-02-08 19:39:22 -0300

    modifique algunas cosas en dev sin querer

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ import Error404 from "./components/pages/Error404";
 import Administrador from "./components/pages/Administrador";
 import Registro from "./components/pages/Registro";
 import Login from "./components/pages/Login";
+import FormularioProducto from "./components/pages/producto/FormularioProducto";
 const App = () => {
   return (
     <BrowserRouter>
     <Menu></Menu>
     <Routes>
       <Route exact path='/' element={<Inicio></Inicio>}></Route>
-      <Route  path="/admin" element={<Administrador></Administrador>}></Route>
+      <Route  path="/admin/" element={<Administrador></Administrador>}></Route>
       <Route path="/registro" element={<Registro></Registro>} ></Route>
       <Route path="/Login" element={<Login></Login>}></Route>
       <Route path="*" element={<Error404></Error404>}></Route>
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,16 +1,16 @@
-import { Navbar,Nav } from "react-bootstrap";
+
 import logo from "../../assets/Coffee_Logo.png";
 import { Link,NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div >
-      <div className="bg-footer text-center py-4 text-light fw-bold">
+      <div className="bg-footer text-center py-3 text-light fw-bold">
      <NavLink as={Link} to='/'>
      <img as 
               src={logo}
               alt="RollingCode Logo nav"
-              className="img-fluid"
+              className="img-fluid "
               width={200}
             />
      </NavLink>
